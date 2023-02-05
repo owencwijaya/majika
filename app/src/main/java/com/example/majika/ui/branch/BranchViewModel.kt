1de388c70e5a5cb6ec5932814b63048652aba114
owencwijaya <owencwijaya@gmail.com>
2023-02-05 21:53:44 +0700

    Revert "chore: create retrofit client and adapter for branch"
    This reverts commit 2c0006a5cec10cf6af1054ceb66e80a6d20472a5.

--- app/src/main/java/com/example/majika/ui/branch/BranchViewModel.kt
+++ app/src/main/java/com/example/majika/ui/branch/BranchViewModel.kt
@@ -1,34 +1,13 @@
 package com.example.majika.ui.branch
 
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.majika.model.Branch
-import com.example.majika.model.BranchList
-import com.example.majika.utils.NetworkConfig
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-
 
 class BranchViewModel : ViewModel() {
 
-    private val _branchList = MutableLiveData<BranchList>().apply {
-        NetworkConfig().getService().getBranch().enqueue(
-            object: Callback<BranchList> {
-                override fun onFailure(call : Call<BranchList>, t: Throwable) {
-                    t.printStackTrace()
-                }
-
-                override fun onResponse(call: Call<BranchList>, response: Response<BranchList>) {
-                    if (response.isSuccessful) {
-                        System.out.println(response.body())
-                        value = response.body()
-                    }
-                }
-            }
-        )
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is branch Fragment"
     }
-    val branchList: LiveData<BranchList> = _branchList
+    val text: LiveData<String> = _text
 }
