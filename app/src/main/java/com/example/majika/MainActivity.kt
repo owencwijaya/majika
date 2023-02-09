bac331244a270fb23fb0f1cc51546945b754fd0c
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-02-09 20:46:02 +0700

    trying to understand. changing a few things, as a way to understand

--- app/src/main/java/com/example/majika/MainActivity.kt
+++ app/src/main/java/com/example/majika/MainActivity.kt
@@ -12,26 +12,22 @@ import com.example.majika.databinding.ActivityMainBinding
 import com.example.majika.ui.cart.CartViewModel
 import com.example.majika.ui.cart.CartViewModelFactory
 
+
+//this is the entry point of the app.
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val cartViewModel: CartViewModel by viewModels{CartViewModelFactory((application as MajikaApplication).repository)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//       initialize view
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//       initialize bottom navigation view and bottom navigation controller, and merge them
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_menu, R.id.navigation_twibbon, R.id.navigation_cart, R.id.navigation_branch
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 }
